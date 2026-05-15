ab52ca79a8d18dedd0175add05215f53acbd27b0
Roberval Cunha <robervalcunhas3@gmail.com>
2026-05-15 15:51:40 -0300

    Adicionar Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
-  title: "GuaraLink | Internet Fibra Óptica em Guaraqueçaba",
+  title: "GuaraLink - Internet Fibra Óptica",
   description:
     "Internet fibra óptica em Guaraqueçaba, Superagui, Medeiros e Massarapuã.",
 };
@@ -14,7 +18,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KQVCVLRDLR"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-KQVCVLRDLR');
+          `}
+        </Script>
+      </head>
+
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
