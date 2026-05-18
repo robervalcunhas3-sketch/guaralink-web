aaefe4bdd12b11ff6c34b9b9fd44310e95f622f0
Roberval Cunha <robervalcunhas3@gmail.com>
2026-05-18 11:20:22 -0300

    Adicionar central cliente e pre cadastro

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const whatsapp =
   "https://wa.me/5541996129713?text=Olá! Quero contratar a internet da GuaraLink.";
 
 const cliente =
-   "https://guaralinkguaraquecaba.beesweb.com.br/pre-cadastro";
+     "https://guaralinkguaraquecaba.beesweb.com.br";
 
 const planosGuaraquecaba = [
   { nome: "300 MEGA", preco: "100", destaque: false },
